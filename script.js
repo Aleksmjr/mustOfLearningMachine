18f3d0c9f8ad07508d513285fb9be9af78f0f7a1
Aleksmjr <Aleksmjr97@gmail.com>
2024-12-02 23:48:45 +0300

    Feat: добавлены переменные и их вывод в консоль

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-let money;
+let money = 45000;
 
-let income;
+let income = 'фриланс';
 
-let addExpenses;
+let addExpenses = 'Интернет, такси, коммуналка';
 
-let deposit;
+let deposit = true;
 
-let mission = 'Заставить Сема сосать хуй';
+let mission = 200000;
 
-let period;
-alert('Сема лох');
-console.log(mission);
+let period = 12;
+// Вывод типа данных
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+//вывод длины строки
+console.log(addExpenses.length);
+//Вывод в консоль строку
+console.log(
+  `Период равен ${period} месяцев, цель заработать ${mission} рублей`,
+);
+//Нижний регистр, разбить на массив и вывел в консоль
+let arr = addExpenses.toLowerCase();
+let expenses = arr.split(' ');
+console.log(expenses);
+// вывод дневного дохода
+let budgetDay = money / 30;
+console.log(budgetDay);
