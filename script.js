b8204e6c2341afba91f2b4f83a0c042f170a93d8
Aleksmjr <Aleksmjr97@gmail.com>
2024-12-06 01:54:29 +0300

    Fixed: function and variables

--- script.js
+++ script.js
@@ -37,7 +37,12 @@ console.log(addExpenses);
 // add promt 'your deposit' and set boolean value
 let deposit = confirm('Есть ли у вас депозит в банке?');
 console.log(deposit); // WE CAN ADDED let deposit1
-console.log(typeof deposit);
+let showTypeOf = function (data) {
+  console.log(data, typeof data);
+};
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
 
 //ask all questions twice
 
@@ -47,10 +52,13 @@ let amount1 = Number(prompt('Во сколько это обойдется?'));
 let amount2 = Number(prompt('Во сколько это обойдется?'));
 console.log(amount1, amount2);
 
+var array1 = (amount1 && '').split('');
+var array2 = (amount2 && '').split('');
+console.log(amount1, amount2);
 // DELETED LET = BudgetMonth
 
 // calc mission 'how many month u need to completed your mission'
-let budgetMission = Math.ceil(mission / budgetMonth);
+// let budgetMission = Math.ceil(mission / budgetMonth);
 
 // console.log(`До накопления средств необходимо ${budgetMission} месяца/ев`);
 
@@ -59,32 +67,40 @@ let budgetMission = Math.ceil(mission / budgetMonth);
 // console.log(budgetDay);
 
 // construction of conditions
-if (budgetDay >= 1200) {
-  console.log('У вас высокий уровень дохода');
-} else if (600 <= budgetDay < 1200) {
-  console.log('У вас средний уровень дохода');
-} else if (600 > budgetDay >= 0) {
-  console.log('К сожалению у вас уровень дохода ниже среднего');
-} else if (budgetDay < 0) {
-  console.log(
-    'Что-то пошло не так, возможно вы ввели не те значения, попробуйте еще раз',
-  );
+
+// Function return all sums (or cums in your throath :D)
+function getExpensesMonth(exp1, exp2) {
+  return exp1 + exp2;
 }
-// Function return all sums (or cums in tour throath :D)
-let sums = function getExpensesMonth() {
-  return amount1 + amount2;
-};
+
 // your money for month - incomes?
-function getAccumulatedMonth() {
-  return budgetMonth - sums;
+function getAccumulatedMonth(budget, exp1, exp2) {
+  return budget - getExpensesMonth(exp1, exp2);
 }
+
 // added func in variable
-let accumulatedMonth = getAccumulatedMonth();
+let accumulatedMonth = getAccumulatedMonth(money, amount1, amount2);
 //
-function getTargetMonth() {
-  return mission / accumulatedMonth;
+function getTargetMonth(target, incomePerMonth) {
+  return target / incomePerMonth;
 }
+console.log(getTargetMonth(mission, accumulatedMonth));
 
 let budgetDay = accumulatedMonth / 30;
-
+console.log(budgetDay);
 //HARDMODE
+let getStatusIncome = function () {
+  if (budgetDay >= 1200) {
+    return 'У вас высокий уровень дохода';
+  }
+  if (600 <= budgetDay < 1200) {
+    return 'У вас средний уровень дохода';
+  }
+  if (600 > budgetDay >= 0) {
+    return 'К сожалению у вас уровень дохода ниже среднего';
+  }
+  if (budgetDay < 0) {
+    return 'Что-то пошло не так, возможно вы ввели не те значения, попробуйте еще раз';
+  }
+};
+console.log(getStatusIncome());
